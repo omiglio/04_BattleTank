2f178b83b2d8bf18ba0b9f855a58c5fab3352761
omiglio <ogalvan5@student.mtsac.edu>
2021-03-10 20:18:12 -0800

    BT21 Delegating to Components

--- BattleTank/Intermediate/Build/Win64/UE4Editor/Inc/BattleTank/BattleTank.init.gen.cpp
+++ BattleTank/Intermediate/Build/Win64/UE4Editor/Inc/BattleTank/BattleTank.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeBattleTank_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x028A2B39,
-				0x80AB06B0,
+				0x75A55E54,
+				0x48BDCEB6,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
